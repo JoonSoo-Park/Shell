a2a30bea5c89396c9555782f3c17373f70400884
JoonSoo <joonsoo181005@gmail.com>
2020-01-09 08:26:19 +0900

    first commit

---
+++ Main.c
@@ -0,0 +1,206 @@
+#include <sys/wait.h>
+#include <sys/types.h>
+#include <unistd.h>
+#include <stdlib.h>
+#include <stdio.h>
+#include <string.h>
+
+int lsh_cd(char **args);
+int lsh_help(char **args);
+int lsh_exit(char **args);
+void lsh_loop(void);
+char *lsh_read_line(void);
+char **lsh_split_line(char *line);
+int lsh_execute(char **args);
+
+
+/*
+ * List of builtin commands, followed by their corresponding functions
+ */
+char *builtin_str[] = {
+	"cd",
+	"help",
+	"exit"
+};
+
+int (*builtin_func[]) (char **) = {
+	&lsh_cd,
+	&lsh_help,
+	&lsh_exit
+};
+
+int lsh_num_builtins() {
+	return sizeof(builtin_str) / sizeof(char *);
+}
+
+int lsh_cd(char **args)
+{
+	if (args[1] == NULL) {
+		fprintf(stderr, "lsh: expected argument to \"cd\"\n");
+	} else {
+		if (chdir(args[1]) != 0) {
+			perror("lsh");
+		}
+	}
+	return 1;
+}
+
+/*
+ * brief builtin command: print help.
+ * param args List of args. Not examined.
+ * return Always return 1, to continue executing
+ */
+int lsh_help(char **args)
+{
+	int i;
+	printf("Type program names and arguments, and hit enter.\n");
+	printf("The following are built in:\n");
+
+	for (i = 0; i < lsh_num_builtins(); ++i) {
+		printf(" %s\n", builtin_str[i]);
+	}
+
+	printf("Use the man command for information on other programs.\n");
+	return 1;
+}
+
+
+/*
+ * brief builtin command: print exit.
+ * param args List of args. Not examined.
+ * return Always return 0, to terminate execution.
+ */
+int lsh_exit(char **args)
+{
+	return 0;
+}
+
+/*
+ * brief launch a program and wait for it to terminate.
+ * param args Null terminated list of arguments (including program).
+ * return Always return 1, to continue execution.
+ */
+int lsh_launch(char **args)
+{
+	pid_t pid, wpid;
+	int status;
+
+	pid = fork();
+	if (pid == 0) {
+		if (execvp(args[0], args) == -1) {
+			perror("lsh");
+		}
+		exit(EXIT_FAILURE);
+	} else if (pid < 0) {
+		perror("lsh");
+	} else {
+		do {
+			wpid = waitpid(pid, &status, WUNTRACED);
+		} while (!WIFEXITED(status) && !WIFSIGNALED(status));
+	}
+	
+	return 1;
+}
+
+void lsh_loop(void)
+{
+	char *line;
+	char **args;
+	int status;
+
+	do {
+		printf("> ");
+		line = lsh_read_line();
+		args = lsh_split_line(line);
+		status = lsh_execute(args);
+
+		free(line);
+		free(args);
+	} while (status);
+}
+
+#define LSH_RL_BUFSIZE 1024
+
+char *lsh_read_line(void)
+{
+	int bufsize = LSH_RL_BUFSIZE;
+	int position = 0;
+	char *buffer = malloc(sizeof(char) * bufsize);
+	int c;
+
+	if (!buffer) {
+		fprintf(stderr, "lsh: allocation error\n");
+		exit(EXIT_FAILURE);
+	}
+
+	while (1) {
+		c = getchar();
+
+		if (c == EOF || c == '\n') {
+			buffer[position] = '\0';
+			return buffer;
+		} else {
+			buffer[position++] = c;
+		}
+
+		if (position >= bufsize) {
+			bufsize += LSH_RL_BUFSIZE;
+			buffer = realloc(buffer, bufsize);
+			if (!buffer) {
+				fprintf(stderr, "lsh: allocation error\n");
+				exit(EXIT_FAILURE);
+			}
+		}
+	}
+}
+
+#define LSH_TOK_BUFSIZE 64
+#define LSH_TOK_DELIM " \t\r\n\a"
+char **lsh_split_line(char *line)
+{
+	int bufsize = LSH_TOK_BUFSIZE, position = 0;
+	char **tokens = malloc(bufsize * sizeof(char*));
+	char *token, **tokens_backup;
+
+	if (!tokens) {
+		fprintf(stderr, "lsh: allocation error\n");
+		exit(EXIT_FAILURE);
+	}
+
+	token = strtok(line, LSH_TOK_DELIM);
+	while (token != NULL) {
+		tokens[position++] = token;
+
+		if (position >= bufsize) {
+			bufsize += LSH_TOK_BUFSIZE;
+			tokens_backup = tokens;
+			tokens = realloc(tokens, bufsize * sizeof(char*));
+			if (!tokens) {
+				free(tokens_backup);
+				fprintf(stderr, "lsh: allocation error\n");
+				exit(EXIT_FAILURE);
+			}
+		}
+
+		token = strtok(NULL, LSH_TOK_DELIM);
+	}
+	tokens[position] = NULL;
+	return tokens;
+}
+
+int main(int argc, char *argv[])
+{
+	// Load config files, if any.
+	
+	// Run command loop.
+	
+	//lsh_loop();
+
+	// Perform any shutdown/cleanup.
+	//
+	//
+	//
+	//
+	
+	return EXIT_SUCCESS;
+}
