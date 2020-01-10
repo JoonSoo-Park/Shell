cc3dc6f9127f56c1f0453783718a8edd1b0b97dc
JoonSoo <joonsoo181005@gmail.com>
2020-01-10 10:58:00 +0900

    added execute(), launch()

--- Main.c
+++ Main.c
@@ -102,6 +102,26 @@ int lsh_launch(char **args)
 	return 1;
 }
 
+int lsh_execute(char **args)
+{
+	int i;
+
+	if (args[0] == NULL) {
+		// An empty command was entered.
+		return 1;
+	}
+
+	// if exists, then runs builtin function.
+	for (i = 0; i < lsh_num_builtins(); ++i) {
+		if (strcmp(args[0], builtin_str[i]) == 0) {
+			return (*builtin_func[i])(args);
+		}
+	}
+
+	// launch program
+	return lsh_launch(args);
+}
+
 void lsh_loop(void)
 {
 	char *line;
@@ -188,13 +208,15 @@ char **lsh_split_line(char *line)
 	return tokens;
 }
 
+
+
 int main(int argc, char *argv[])
 {
 	// Load config files, if any.
 	
 	// Run command loop.
 	
-	//lsh_loop();
+	lsh_loop();
 
 	// Perform any shutdown/cleanup.
 	//
