9fb4af3a9e7bd71deb012c0d65d958a0a2eaa152
JoonSoo <joonsoo181005@gmail.com>
2020-02-11 12:46:20 +0900

    added argsPipe = NULL

--- src2/TestShell.c
+++ src2/TestShell.c
@@ -9,8 +9,8 @@
 #include <readline/readline.h>
 #include <readline/history.h>
 
-// TODO: 
-// 1. pipe
+//TODO:
+//builtins
 
 #define TRUE 1
 #define FALSE 0
@@ -24,17 +24,20 @@
 #define BUFFER_SIZE 1024
 
 int lsh_cd(char **args);
+int lsh_history(char **args);
 int lsh_help(char **args);
 int lsh_exit(char **args);
 
 char *builtin_str[] = {
 	"cd",
+	"history",
 	"help",
 	"exit"
 };
 
 int (*builtin_func[]) (char**) = {
 	&lsh_cd,
+	&lsh_history,
 	&lsh_help,
 	&lsh_exit
 };
@@ -51,6 +54,18 @@ int lsh_cd(char **args)
 		if (chdir(args[1]) != 0) {
 			perror("lsh");
 		}
+	}
+	return 1;
+}
+
+int lsh_history(char **args)
+{
+	HISTORY_STATE *myhist = history_get_history_state();	
+	HIST_ENTRY **mylist = history_list();
+
+	for (int i = 0; i < myhist->length; ++i) {
+		printf("%s %s\n", mylist[i]->line, mylist[i]->timestamp);
+		free_history_entry (mylist[i]);
 	}
 	return 1;
 }
@@ -121,6 +136,7 @@ char** lsh_split_line(char* line)
 		token = strtok(NULL, LSH_TOK_DELIM);
 	}
 	tokens[position] = NULL;
+
 	return tokens;
 }
 
@@ -128,8 +144,6 @@ int lsh_launch(char **args)
 {
 	pid_t pid, wpid;
 	int status;
-
-	printf("lsh_launch\n");
 
 	pid = fork();
 	if (pid == 0) {
@@ -173,6 +187,7 @@ int lsh_launch_pipe(char** args, char** argsPipe)
 			return 0;
 		}
 	} else {
+			close(pipefd[1]);
 		p2 = fork();
 		if (p2 < 0) {
 			fprintf(stderr, "Frok failed");
@@ -219,7 +234,6 @@ int lsh_execute(char **args, char** argsPipe)
 
 int lsh_split_pipe(char** line, char** pipe)
 {
-	// TODO:
 	// if no input returns -1
 	// if piped returns 1
 	// if not piped return 0
@@ -235,10 +249,6 @@ int lsh_split_pipe(char** line, char** pipe)
 
 void lsh_loop(void)
 {
-	// TODO
-	// 1. read line
-	// 2. check if it has pipe
-	// 3. if it has pipe then seperate it.
 	char *line, *pipe;
 	char **args = NULL, **argsPipe = NULL;
 	int status, piped;
@@ -259,7 +269,11 @@ void lsh_loop(void)
 
 		free(line);
 		free(args);
-		//free(argsPipe);
+		free(argsPipe);
+
+		line = NULL;
+		pipe = NULL;
+		argsPipe = NULL;
 	} while(status);
 }
 
